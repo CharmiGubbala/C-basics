fdfdc1161f57094328e92f2c7b67cd377daa77e0
CharmiGubbala <147477043+CharmiGubbala@users.noreply.github.com>
2024-01-02 10:33:31 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,x;
+	a=1;
+	b=2;
+	x=a+b;
+	printf("addition of %d and %d is %d\n",a,b,x);
+	x=a-b;
+	printf("subtraction of %d and %d is %d\n",a,b,x);
+	x=a*b;
+	printf("multiplication of %d and %d is %d",a,b,x);
+}
